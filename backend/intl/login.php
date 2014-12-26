b4306215923b1e5ac1fc522b291a3a07911349f3
clement <maboiteaspam@wanadoo.fr>
2014-12-26 23:16:57 +0800

    refactored some intl

--- backend/intl/login.php
+++ backend/intl/login.php
@@ -1,13 +1,5 @@
 <?
 return array(
-    "password.must_not_be_empty"=>array(
-        "en"=>"Password must not be empty !",
-        "fr"=>"Mot de passe ne doit pas être vide !",
-    ),
-    "login.must_not_be_empty"=>array(
-        "en"=>"Login must not be empty !",
-        "fr"=>"Nom d'utilisateur ne doit pas être vide !",
-    ),
     "credentials.are_incorrect"=>array(
         "en"=>"Credentials are incorrect !",
         "fr"=>"Vos identifiants sont incorrects !",
